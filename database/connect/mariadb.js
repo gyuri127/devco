10c4a2b5e280da0570415800fb29980a89f87ddd
gyuri127 <117248176+gyuri127@users.noreply.github.com>
2024-09-23 01:33:47 +0900

    database 연동 업데이트!

--- database/connect/mariadb.js
+++ database/connect/mariadb.js
@@ -3,12 +3,11 @@ const mariadb = require('mysql'); //모듈 불러오기
 const conn = mariadb.createConnection( //masql 모듈이 가지고 있는 함수
     {
         host : 'localhost',
-        port :3307,  
+        port :3306,  
         user :'root',
         password: 'root',
         database : 'Tennis'
     }
-
 );
 
 module.exports = conn;  //module로 사용하려고 conn내보냄
